1cabb650db167987953b47c6b35c1c8eb98f780f
max-student <max-student@maxtrain.com>
2020-02-21 16:05:07 -0500

    Add project files.

---
+++ LinqExtensionMethods/Pricing.cs
@@ -0,0 +1,18 @@
+﻿
+        using System;
+        using System.Collections.Generic;
+        using System.Text;
+
+
+namespace LinqAndExtensionMethods {
+
+        public class Pricing {
+
+        public string ProductCode { get; set; }
+        public double Price { get; set; }
+
+    }
+}
+
+
+
---
+++ LinqExtensionMethods/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqExtensionMethods {
+    
+        public class Product {
+
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public bool Taxible { get; set; }
+
+        public object ProductCode { get; internal set; }
+    }
+}
+
+
